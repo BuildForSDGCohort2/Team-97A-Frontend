d2e3090951fd3cd3a2fed0f4120be731a177e32d
abdoulrasheed <abdoulrasheedibrahim47@gmail.com>
2020-08-27 15:52:40 +0100

    Update "features" page

--- src/components/features/features.jsx
+++ src/components/features/features.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import "./features.css";
+import screen from "../../images/screen.svg";
 
 const Features = () => {
   return (
     <div className="features">
       <div></div>
-      <h1>Features</h1>
+      <div>
+        <img alt="demo screen" src={screen} className="screen" />
+      </div>
       <div></div>
+      <div>
+        <ul className="feature-list">
+          <li>
+            <h4>&raquo; Unique Wallet System</h4>
+          </li>
+          <li>
+            <h4>&raquo; Easy to use Cargo tracker</h4>
+          </li>
+          <li>
+            <h4>&raquo; Carrier Rating</h4>
+          </li>
+          <li>
+            <h4>&raquo; Easy Navigation</h4>
+          </li>
+          <li>
+            <h4>&raquo; View Carriers Travelling photos</h4>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
