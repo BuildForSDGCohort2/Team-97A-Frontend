f30175d35a227cc551094fc65a35370c6c811205
abdoulrasheed <abdoulrasheedibrahim47@gmail.com>
2020-08-27 16:04:36 +0100

    Update "features" page

--- src/components/features/features.jsx
+++ src/components/features/features.jsx
@@ -6,26 +6,27 @@ const Features = () => {
   return (
     <div className="features">
       <div></div>
-      <div>
+      <div className="screen-container">
         <img alt="demo screen" src={screen} className="screen" />
+        <h2>Features</h2>
       </div>
       <div></div>
       <div>
         <ul className="feature-list">
           <li>
-            <h4>&raquo; Unique Wallet System</h4>
+            <h4>&raquo; Unique wallet system</h4>
           </li>
           <li>
             <h4>&raquo; Easy to use Cargo tracker</h4>
           </li>
           <li>
-            <h4>&raquo; Carrier Rating</h4>
+            <h4>&raquo; Carrier rating</h4>
           </li>
           <li>
-            <h4>&raquo; Easy Navigation</h4>
+            <h4>&raquo; Easy navigation</h4>
           </li>
           <li>
-            <h4>&raquo; View Carriers Travelling photos</h4>
+            <h4>&raquo; Social space for travellers</h4>
           </li>
         </ul>
       </div>
