72ab94551690ec1f13cbbef60e72caa45f9d2668
abdoulrasheed <abdoulrasheedibrahim47@gmail.com>
2020-08-27 16:54:35 +0100

    Update 'Login Page'

--- src/components/auth/login/login.jsx
+++ src/components/auth/login/login.jsx
@@ -1,12 +1,54 @@
 import React from "react";
 import "./login.css";
+import login_icon from "../../../images/icons/login.svg";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div className="login">
-      <div></div>
-      <h1>Login</h1>
-      <div></div>
+      <div className="icon-container">
+        <h2>Login</h2>
+        <img alt="login into door" src={login_icon} />
+      </div>
+      <div className="login-form-container">
+        <form method="post" className="login-form">
+          <div>
+            <h4>Sign into your account</h4>
+          </div>
+          <div>
+            <input
+              className="text-input"
+              type="text"
+              name="username"
+              placeholder="Email"
+            />
+          </div>
+          <div>
+            <input
+              className="text-input"
+              type="password"
+              name="username"
+              placeholder="Password"
+            />
+          </div>
+          <div>
+            <input
+              type="submit"
+              className="btn-login"
+              name="submit"
+              value="Login"
+            />
+          </div>
+          <div className="sub-login-area">
+            <span>
+              <Link to="/getstarted/">Register</Link>
+            </span>
+            <span>
+              <Link to="/forgotpassword/">Forgot password ?</Link>
+            </span>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
