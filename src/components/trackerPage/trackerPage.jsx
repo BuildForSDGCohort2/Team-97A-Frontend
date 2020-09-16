3e26af65089c04ce6995b764603d4dc43225a4a8
JeremAIh <esiebomaj@gmail.com>
2020-09-16 13:51:15 +0100

    fully added tracker for carrier

--- src/components/trackerPage/trackerPage.jsx
+++ src/components/trackerPage/trackerPage.jsx
@@ -3,15 +3,18 @@ import closeIcon from "../../images/dashboard/close.png";
 import profileImg from "../../images/dashboard/profile-photo1.png";
 import TrackerAction from "./trackerAction";
 import Tracker from "./tracker";
+import PinForm from "./pinForm";
+import Rating from "./rating";
 import "./trackerPage.css";
-import e from "express";
 
 class TrackerPage extends Component {
   state = {
     tracker: { confirmed: true, inTransit: false, delivered: false },
-    status: "inTransit",
+    trackerStatus: "confirmed",
     pin: { pin1: "", pin2: "", pin3: "", pin4: "", pin5: "" },
+    rating: 3,
   };
+
   handleDetailClose = () => {
     this.props.history.push("/packages/");
   };
@@ -28,7 +31,9 @@ class TrackerPage extends Component {
   };
 
   handleConfirmDelivery = () => {
-    console.log("confirming delivery");
+    const pin = Object.values(this.state.pin).join("");
+    console.log(pin);
+    //send pin to backend here
   };
 
   render() {
@@ -44,9 +49,16 @@ class TrackerPage extends Component {
           />
 
           <div className="description">
-            <h4>
-              please call the owner to confirm your misssion and schedule pickup
-            </h4>
+            {this.state.trackerStatus === "confirmed" ? (
+              <h4>
+                please call the owner to confirm your misssion and schedule
+                pickup
+              </h4>
+            ) : this.state.trackerStatus === "inTransit" ? (
+              <h4>please request delivery code from</h4>
+            ) : (
+              <h4>Package successfully delivered to</h4>
+            )}
           </div>
 
           <div className="sender">
@@ -66,54 +78,22 @@ class TrackerPage extends Component {
               </div>
             </div>
           </div>
-          <div className="pin-input">
-            <input
-              type="number"
-              name="pin1"
-              id="pin1"
-              placeholder="-"
-              value={this.state.pin.pin1}
-              onInput={this.handlePinChange}
-            />
-            <input
-              type="number"
-              name="pin2"
-              id="pin2"
-              placeholder="-"
-              value={this.state.pin.pin2}
-              onInput={this.handlePinChange}
-            />
-            <input
-              type="number"
-              name="pin3"
-              id="pin3"
-              placeholder="-"
-              value={this.state.pin.pin3}
-              onInput={this.handlePinChange}
-            />
-            <input
-              type="number"
-              name="pin4"
-              id="pin4"
-              placeholder="-"
-              value={this.state.pin.pin4}
-              onInput={this.handlePinChange}
-            />
-            <input
-              type="number"
-              name="pin5"
-              id="pin5"
-              placeholder="-"
-              value={this.state.pin.pin5}
-              onInput={this.handlePinChange}
-            />
-          </div>
+
+          <PinForm
+            onPinChange={this.handlePinChange}
+            pin={this.state.pin}
+            trackerStatus={this.state.trackerStatus}
+          />
           <TrackerAction
-            status={this.state.status}
+            status={this.state.trackerStatus}
             onConfirmCollection={this.handleConfirmCollection}
             onConfirmDelivery={this.handleConfirmDelivery}
           />
           <Tracker tracker={this.state.tracker} />
+          <Rating
+            rating={this.state.rating}
+            trackerStatus={this.state.trackerStatus}
+          />
         </div>
       </div>
     );
